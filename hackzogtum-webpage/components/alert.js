4db6932516ae5d9bce3c0f145a58acdfd2f93153
Valentin Kummert <v.kummert@drmnt.de>
2020-12-13 00:17:23 +0100

    test some functions of nextjs

--- hackzogtum-webpage/components/alert.js
+++ hackzogtum-webpage/components/alert.js
@@ -14,25 +14,9 @@ export default function Alert({ preview }) {
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
-              This page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
             </>
           )}
         </div>
