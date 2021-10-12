92ea4d1bb6bc2ed0e7da7f1107b17063218b33b0
Tobias Wich <tobias.wich@ecsec.de>
2021-10-12 23:03:34 +0200

    Add beitritt page

---
+++ hackzogtum-webpage/components/beitritt.js
@@ -0,0 +1,35 @@
+export default function Beitritt() {
+    return (
+      <info>
+            <h3>Tritt bei</h3>     
+            <div class="entry-content">
+                <p>Schön, dass du den Weg hierher gefunden hast. Es sind nur noch ein paar kleine Schritte bis auch du Teil des Hackzogtum Coburg bist.</p> <br />
+
+                <p><b>0. Lies die <a href="/assets/Satzung_Hackzogtum_Coburg_e.V._2015-05-09.pdf" target="_blank" rel="noopener">Satzung</a>.</b></p>
+                <br />
+
+                <p><b>1. Lade den Mitgliedsantrag als <a href="/assets/Mitgliedsantrag_eForm.pdf" target="_blank" rel="noopener">eFormular </a> 
+				oder als <a href="/assets/Mitgliedsantrag_print1.pdf" target="_blank" rel="noopener">Standard-PDF </a> herunter.</b><br />
+                (Nutze das Standard-PDF, wenn dein PDF-Reader keine Formulare unterstützt)</p>
+                <br />
+
+                <p><b>2. Fülle den Mitgliedsantrag aus.</b><br /></p>
+                <br />
+
+                <p><b>3. Drücke den ausgefüllten Antrag bei einem unserer Treffen einem Vorstand in die Hand – oder schicke ihn klassisch per Post an:</b><br />
+                Hackzogtum Coburg e.V.<br/>
+				Florian Otto (1. Vorsitzender)<br/>
+				Kirchwegäcker 6<br/>
+				96484 Meeder</p>
+                <br />
+
+                <p><b>4. Bezahle deinen Mitgliedsbeitrag.</b><br />
+                Der Mitgliedsbeitrag beträgt aktuell 20 Euro pro Monat. In Ausnahmen kann der Vorstand einem geringeren Mitgliedsbeitrag zustimmen. 
+				In der Regel gilt dies für Schüler, Studenten, Auszubildende, Bundesfreiwilligendienstleistende und Arbeitslose.</p>
+                <br />
+
+                <p><b>5. Bringe dich ein.</b><br /></p>
+            </div>
+      </info>
+    )
+  }
