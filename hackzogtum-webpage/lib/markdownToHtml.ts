e7617ae81941cb41c36af095ac3d30c59b444f4f
Tobias Wich <tobias.wich@electrologic.org>
2021-11-10 14:07:14 +0100

    Migrate all source files to typescript

--- hackzogtum-webpage/lib/markdownToHtml.ts
+++ hackzogtum-webpage/lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import remark from 'remark'
 import html from 'remark-html'
+import { VFile } from 'vfile'
 
-export default async function markdownToHtml(markdown) {
+export default async function markdownToHtml(markdown: VFile) {
   const result = await remark().use(html).process(markdown)
   return result.toString()
 }
