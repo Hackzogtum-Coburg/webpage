ae9509d0dd56bdf1a983fc9a5ab30514ab0ad955
Tobias Wich <tobias.wich@ecsec.de>
2022-04-03 11:48:07 +0200

    Add complete URL to feed

--- hackzogtum-webpage/lib/blog-utils.ts
+++ hackzogtum-webpage/lib/blog-utils.ts
@@ -5,7 +5,7 @@ import { GetStaticProps } from 'next';
 import remark from 'remark'
 import html from 'remark-html'
 import { VFileCompatible } from 'vfile'
-import { BLOG_TITLE } from './constants';
+import { BLOG_TITLE, BLOG_URL } from './constants';
 
 
 const postsDirectory = join(process.cwd(), '_posts');
@@ -85,6 +85,7 @@ export function getAllPostsProps(from: number, to?: number): GetStaticProps<Post
     const slicedPosts = allPosts.slice(from, to);
 
     // write feed.xml file
+    // TODO: figure out if this can be done without building the file in the source tree
     const rss = await generateRss(allPosts)
     fs.writeFileSync('./public/feed.xml', rss)
   
@@ -98,10 +99,10 @@ export function getAllPostsProps(from: number, to?: number): GetStaticProps<Post
 export async function generateRssItem(post: IPost) {
   return `
     <item>
-      <guid>/posts/${post.slug}</guid>
+      <guid>${BLOG_URL}/posts/${post.slug}</guid>
       <title>${post.title}</title>
       <!--description>{post.excerpt}</description-->
-      <link>/posts/${post.slug}</link>
+      <link>${BLOG_URL}/posts/${post.slug}</link>
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
       <content:encoded><![CDATA[${post.content_html}]]></content:encoded>
     </item>
@@ -116,11 +117,11 @@ export async function generateRss(posts: IPost[]) {
     <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
       <channel>
         <title>${BLOG_TITLE}</title>
-        <!--link>{BLOG_URL}</link-->
+        <link>${BLOG_URL}/</link>
         <!--description>{BLOG_SUBTITLE}</description-->
         <language>en</language>
         <lastBuildDate>${lastDate}</lastBuildDate>
-        <!--atom:link href="{BLOG_URL}" rel="self" type="application/rss+xml"/-->
+        <atom:link href="${BLOG_URL}/" rel="self" type="application/rss+xml"/>
         ${itemsList.join('')}
       </channel>
     </rss>
