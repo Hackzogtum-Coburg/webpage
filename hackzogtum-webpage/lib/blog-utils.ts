e894081cc80f643232ee68f877a2b5316b7d63fa
Tobias Wich <tobias.wich@ecsec.de>
2022-04-03 11:42:42 +0200

    Generate basic RSS feed

--- hackzogtum-webpage/lib/blog-utils.ts
+++ hackzogtum-webpage/lib/blog-utils.ts
@@ -5,6 +5,7 @@ import { GetStaticProps } from 'next';
 import remark from 'remark'
 import html from 'remark-html'
 import { VFileCompatible } from 'vfile'
+import { BLOG_TITLE } from './constants';
 
 
 const postsDirectory = join(process.cwd(), '_posts');
@@ -82,9 +83,46 @@ export function getAllPostsProps(from: number, to?: number): GetStaticProps<Post
   return async (context) => {
     const allPosts = await getAllPosts()
     const slicedPosts = allPosts.slice(from, to);
+
+    // write feed.xml file
+    const rss = await generateRss(allPosts)
+    fs.writeFileSync('./public/feed.xml', rss)
   
     return {
       props: { allPosts: slicedPosts },
     }
   };
+}
+
+
+export async function generateRssItem(post: IPost) {
+  return `
+    <item>
+      <guid>/posts/${post.slug}</guid>
+      <title>${post.title}</title>
+      <!--description>{post.excerpt}</description-->
+      <link>/posts/${post.slug}</link>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+      <content:encoded><![CDATA[${post.content_html}]]></content:encoded>
+    </item>
+  `
+}
+
+export async function generateRss(posts: IPost[]) {
+  const itemsList = await Promise.all(posts.map(generateRssItem))
+  let lastDate = new Date(posts[0].date).toUTCString();
+
+  return `
+    <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
+      <channel>
+        <title>${BLOG_TITLE}</title>
+        <!--link>{BLOG_URL}</link-->
+        <!--description>{BLOG_SUBTITLE}</description-->
+        <language>en</language>
+        <lastBuildDate>${lastDate}</lastBuildDate>
+        <!--atom:link href="{BLOG_URL}" rel="self" type="application/rss+xml"/-->
+        ${itemsList.join('')}
+      </channel>
+    </rss>
+  `
 }
