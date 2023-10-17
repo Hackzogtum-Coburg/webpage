7107abb89507b05549376342917fd2d69533a933
Valle <valentin@degaso.de>
2023-10-17 23:07:59 +0200

    first steps adding the inlist

--- hackzogtum-webpage/components/intro.tsx
+++ hackzogtum-webpage/components/intro.tsx
@@ -2,8 +2,29 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faPhotoVideo, faInfoCircle, faCodeBranch, faAddressBook, faUserSecret } from "@fortawesome/free-solid-svg-icons"; 
+import { Key, ReactChild, ReactFragment, ReactPortal, useEffect, useState } from 'react';
+
+
 
 export default function Intro() {
+
+  const [data, setData] = useState<{ api: string, sensors: any } | null>(null);
+
+  useEffect(() => {
+    fetch('https://spaceapi.hackzogtum-coburg.de/')
+      .then((response) => response.json())
+      .then((data) => {
+        // Hier kannst du mit den API-Daten arbeiten
+        console.log(data);
+        // Setze den API-Rückgabewert in einen State, um ihn in der Komponente anzuzeigen
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Fehler bei der API-Anfrage:', error);
+      });
+  }, []);
+
+
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12 headBorder">
       <a className="hiddenLink" rel="me" href="https://chaos.social/@Hackzogtum">Mastodon</a>
@@ -17,6 +38,17 @@ export default function Intro() {
         />
         </div>
       </h1>
+
+
+      {data && (
+        <div>
+          {data.sensors["in space"].map((item: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined, index: Key | null | undefined) => (
+            <h1 key={index}>{item}</h1>
+          ))}
+        </div>
+      )}
+
+
       <h4 className="menuBtns">
         <div className="btnCluster">
           {' '}
